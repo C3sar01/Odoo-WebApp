13676e9c7b5b71998a2b99e6de908fa633ee024a
Lucas-byte981 <lukas.mardonesv@gmail.com>
2020-12-20 18:32:08 -0300

    cambio en archivo py y en vista

--- venta/models/boleta.py
+++ venta/models/boleta.py
@@ -6,10 +6,7 @@ class Boleta(models.Model):
      _name = 'venta.boleta'
 
      fecha= fields.Date()
-     subtotal= fields.Integer(default=0)
      descuento = fields.Float(default=0)
-     total= fields.Float(default=0)
-
      detalle_boleta_ids = fields.One2many(
          'venta.detalle_boleta', 'boleta_id', string="Detalle de la venta")
 
@@ -19,9 +16,8 @@ class DetalleBoleta(models.Model):
     precio = fields.Integer()
     sub_total = fields.Integer(String="Sub Total", compute="sub_total")
     descuento= fields.Integer(default=0)
-    total = fields.Integer(string="Total", compute="total_detalle_boleta")
     boleta_id= fields.Many2one('venta.boleta', String="Boleta")
-    
+
     @api.one
     def _sub_total(self):
         self.sub_total = (self.cantidad*self.precio)
