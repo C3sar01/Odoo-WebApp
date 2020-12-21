ecdf77fac5ad241c3f7fc1d5de95be569dcf4bfa
Lucas-byte981 <lukas.mardonesv@gmail.com>
2020-12-21 02:10:20 -0300

    modificacion del archivo .py y actualizacion de las vistas

--- venta/models/boleta.py
+++ venta/models/boleta.py
@@ -5,24 +5,28 @@ from odoo import models, fields, api
 class Boleta(models.Model):
      _name = 'venta.boleta'
 
-     local= fields.Char (string="Nombre del local", required=True)
      fecha= fields.Date()
+     sucursal= fields.Char (string="Sucursal", required=True)
+     direccion= fields.Char(string="Dirección", required=True)
+     caja=fields.Integer(string="Caja")
+     cajero= fields.Char (string="Cajero", required=True)
  
      detalle_boleta_ids = fields.One2many(
          'venta.detalle_boleta', 'boleta_id', string="Detalle de la venta")
 
 class DetalleBoleta(models.Model):
     _name = 'venta.detalle_boleta'
-    
+
+    producto= fields.Many2one('inventario.producto', String="Producto")
     cantidad = fields.Integer(default=1)
-    precio = fields.Integer()
+    precio_unitario = fields.Integer()
     descuento= fields.Integer(default=0)
-    sub_total = fields.Integer(String="Sub Total", compute="sub_total")
+    sub_total = fields.Integer(String="Sub Total", compute="_sub_total")
     boleta_id= fields.Many2one('venta.boleta', String="Boleta")
 
     @api.one
     def _sub_total(self):
-        self.sub_total = (self.cantidad*self.precio)
+        self.sub_total = (self.cantidad*self.precio_unitario)
 
     @api.one
     def _total(self):
